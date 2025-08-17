fdf54bad1a87551e5a667f78587436a61a63c6c8
Andrew Teles <andrewggst@gmail.com>
2025-08-17 12:07:18 -0300

    Create  functions.swift

---
+++ logicaSwift/projetoToDoList/functions.swift
@@ -0,0 +1,48 @@
+func listar (lista: [String]){
+    print("")
+    print("Suas tarefas:")
+    for i in 0..<lista.count {
+        print("\(i+1) - \(lista[i])")
+    }
+}
+
+func adicionar (lista: [String], tarefa: String) -> [String] {
+    var listaCopia = lista
+    listaCopia.append(tarefa)
+    return listaCopia
+}
+
+func remover (lista: [String], tarefa: String) -> [String]{
+    var listaCopia = lista
+    var indice: Int = 0
+    for i in 0..<listaCopia.count {
+        if listaCopia[i] == tarefa {
+            indice = i
+        }
+    }
+    listaCopia.remove(at: indice)
+    return listaCopia
+}
+
+func editar (lista: [String], tarefa: String, tarefaEditada: String) -> [String]{
+    var listaCopia = lista
+    var indice: Int = 0
+    for i in 0..<listaCopia.count {
+        if listaCopia[i] == tarefa {
+            indice = i
+        }
+    }
+    listaCopia[indice] = tarefaEditada
+    return listaCopia
+}
+
+func prioridade(lista: [String], indice: Int, indiceNovo: Int) -> [String] {
+    var listaCopia = lista
+    listaCopia.insert(listaCopia[indice], at: indiceNovo)
+    if indiceNovo < indice {
+        listaCopia.remove(at: indice + 1)
+    }else {
+        listaCopia.remove(at: indice)
+    }
+    return listaCopia
+}
